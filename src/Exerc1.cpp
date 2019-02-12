feb9321caf7ab85c527da898f9cd2de0f76a0fbe
baddi <baddiyoussef85@gmail.com>
2019-02-12 11:34:13 +0100

    add excercice 1

--- src/Exerc1.cpp
+++ src/Exerc1.cpp
@@ -12,16 +12,13 @@ using namespace std;
 int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 
-	char c = '\x05';
-	int n = 5;
-	long p = 1000;
-	float x = 1.25;
-	double z = 5.5;
+	char c = '\x01';
+	short int p = 10;
 
-	cout << n + c + p << endl;
-	cout << 2 * x + c << endl;
-	cout << (char) n + c << endl;
-	cout << (float) z + n / 2 << endl;
+	cout << p + 3 << endl;
+	cout << c + 1 << endl;
+	cout << p + c << endl;
+	cout << 3 * p + 5 * c << endl;
 
 	return 0;
 }
